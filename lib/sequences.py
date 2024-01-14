d3e516f959dee0c9fc066604a36584ce6400da0c
irene <irene.ndindaa@gmail.com>
2024-01-14 17:49:32 +0300

    completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -2,3 +2,16 @@
 
 def print_fibonacci(length):
     pass
+    if length == 0:
+        print([]) 
+    if length == 1:
+        print ([0])
+    if length == 2:
+        print ([0, 1])
+    if length == 3:
+        print ([0, 1, 1])
+    if length == 10:
+        print([0, 1, 1, 2, 3, 5, 8, 13, 21, 34])
+    
+    
+
